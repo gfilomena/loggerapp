1c6f49d474a8d68ab443218542bc45c499bf1d74
salsx <salsx@hotmail.it>
2018-12-22 11:15:51 +0100

    fix 3

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.static(__dirname + '/dist'));
 
 
 // Catch all other routes and return the index file
-  app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+//app.get('/*', (req, res) => {
+  //res.sendFile(path.join(__dirname, 'dist/index.html'));
+//});
 
 
 app.post('/dnslookups', function (req, res) {
