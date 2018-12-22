bc661620f8ffcb321b1d5c3b24c24c2fb66bb8e9
salsx <salsx@hotmail.it>
2018-12-22 12:39:18 +0100

    fix 4

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ import 'hammerjs';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
